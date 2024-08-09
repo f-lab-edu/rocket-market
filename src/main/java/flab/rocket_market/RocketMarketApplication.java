ae8d5094fda988327b4f20912c63509ecc65787a
cheoneunjeong <0102skyjung@naver.com>
2024-08-10 00:18:06 +0900

    [Feature] 프로젝트 생성 #3

---
+++ src/main/java/flab/rocket_market/RocketMarketApplication.java
@@ -0,0 +1,13 @@
+package flab.rocket_market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RocketMarketApplication.class, args);
+	}
+
+}
