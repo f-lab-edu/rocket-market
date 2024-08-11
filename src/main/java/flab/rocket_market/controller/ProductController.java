5daf2164eb91c9f509751c88a8b28e2e988c4cab
cheoneunjeong <0102skyjung@naver.com>
2024-08-12 00:51:04 +0900

    [Feature] 제품 등록, 조회, 수정, 삭제 API 구현 #10

---
+++ src/main/java/flab/rocket_market/controller/ProductController.java
@@ -0,0 +1,43 @@
+package flab.rocket_market.controller;
+
+import flab.rocket_market.controller.dto.ProductResponse;
+import flab.rocket_market.controller.dto.RegisterProductRequest;
+import flab.rocket_market.controller.dto.UpdateProductRequest;
+import flab.rocket_market.global.response.BaseDataResponse;
+import flab.rocket_market.global.response.BaseResponse;
+import flab.rocket_market.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public BaseResponse registerProduct(@RequestBody RegisterProductRequest productRequest) {
+        productService.registerProduct(productRequest);
+        return BaseResponse.of(HttpStatus.CREATED, "성공적으로 등록되었습니다.");
+    }
+
+    @GetMapping("/{productId}")
+    public BaseDataResponse<ProductResponse> getProductById(@PathVariable("productId") Long productId) {
+        ProductResponse product = productService.getProductById(productId);
+        return BaseDataResponse.of(HttpStatus.OK, "성공적으로 조회되었습니다.", product);
+    }
+
+    @PatchMapping
+    public BaseResponse updateProduct(@RequestBody UpdateProductRequest productRequest) {
+        productService.updateProduct(productRequest);
+        return BaseResponse.of(HttpStatus.OK, "성공적으로 수정되었습니다.");
+    }
+
+    @DeleteMapping("/{productId}")
+    public BaseResponse deleteProduct(@PathVariable("productId") Long productId) {
+        productService.deleteProduct(productId);
+        return BaseResponse.of(HttpStatus.NO_CONTENT, "성공적으로 삭제되었습니다.");
+    }
+}
