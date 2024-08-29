93e991b7d2acac611ee851ba82a1e027e5733ba3
cheoneunjeong <0102skyjung@naver.com>
2024-08-29 16:38:54 +0900

    [Feature] 전역예외처리기 유효성 검증 실패 오류 추가 #32

--- src/main/java/flab/rocket_market/global/exception/RocketMarketExceptionAdvice.java
+++ src/main/java/flab/rocket_market/global/exception/RocketMarketExceptionAdvice.java
@@ -1,8 +1,16 @@
 package flab.rocket_market.global.exception;
 
+import flab.rocket_market.global.response.BaseDataResponse;
 import flab.rocket_market.global.response.BaseResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static flab.rocket_market.global.message.MessageConstants.VALIDATION_FAILED;
 
 @RestControllerAdvice
 public class RocketMarketExceptionAdvice {
@@ -10,5 +18,14 @@ public class RocketMarketExceptionAdvice {
     @ExceptionHandler(RocketMarketException.class)
     public BaseResponse handleException(RocketMarketException exception) {
         return BaseResponse.of(exception.getError().getStatus(), exception.getError().getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public BaseDataResponse<Map<String, String>> handleValidationException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage())
+        );
+        return BaseDataResponse.of(HttpStatus.BAD_REQUEST, VALIDATION_FAILED.getMessage(), errors);
     }
 }
