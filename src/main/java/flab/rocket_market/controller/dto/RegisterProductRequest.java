7af89d53922b816f87195166ad7bbd9a80d31bae
cheoneunjeong <0102skyjung@naver.com>
2024-08-29 16:27:06 +0900

    [Feature] DTO 유효성 검사 추가 #32

--- src/main/java/flab/rocket_market/controller/dto/RegisterProductRequest.java
+++ src/main/java/flab/rocket_market/controller/dto/RegisterProductRequest.java
@@ -1,5 +1,7 @@
 package flab.rocket_market.controller.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +15,14 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class RegisterProductRequest {
 
+    @NotBlank(message = "{product.name.notblank}")
     private String name;
+
     private String description;
+
+    @NotNull(message = "{product.price.notnull}")
     private BigDecimal price;
+
+    @NotNull(message = "{product.category.notnull}")
     private Long categoryId;
 }
