6fc816b193fee1d7cf8d2e728562895c4250138e
cheoneunjeong <0102skyjung@naver.com>
2024-08-29 23:13:54 +0900

    [Feature] 품목 조회 부분 테스트 코드 추가 #20

--- src/main/java/flab/rocket_market/controller/ProductController.java
+++ src/main/java/flab/rocket_market/controller/ProductController.java
@@ -12,8 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 import static flab.rocket_market.global.message.MessageConstants.*;
 
 @RestController
