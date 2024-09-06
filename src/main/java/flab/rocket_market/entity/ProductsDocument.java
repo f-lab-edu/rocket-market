90daa342f5b062e6d28ef3c72c7682db67356a60
cheoneunjeong <0102skyjung@naver.com>
2024-09-07 00:16:57 +0900

    [Feature] elasticsearch 인덱스 관련 클래스 추가 #48

---
+++ src/main/java/flab/rocket_market/entity/ProductsDocument.java
@@ -0,0 +1,45 @@
+package flab.rocket_market.entity;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * 엘라스틱 서치에 사용 되는 인덱스 (관계형 디비 table - 엘라스틱서치 index)
+ * */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+@Setting(settingPath = "/static/elastic/elastic-settings.json")
+@Mapping(mappingPath = "/static/elastic/product-mappings.json")
+@Document(indexName = "products")
+public class ProductsDocument {
+
+    @Id
+    @Field(name = "product_id", type = FieldType.Long)
+    private Long productsId;
+
+    @Field(type = FieldType.Text)
+    private String name;
+
+    @Field(type = FieldType.Text)
+    private String description;
+
+    @Field(type = FieldType.Scaled_Float, scalingFactor = 100)
+    private BigDecimal price;
+
+    @Field(type = FieldType.Text)
+    private String categoryName;
+
+    @Field(name = "createdAt", type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime createdAt;
+
+    @Field(name = "updated_at", type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime updatedAt;
+}
