8b0a059ce1363d399c51d0be0268be0c725a3ca4
cheoneunjeong <0102skyjung@naver.com>
2024-09-14 12:11:44 +0900

    [Feature] products 관련 클래스 패키지 분리 #57

---
+++ src/main/java/flab/rocket_market/products/entity/Categories.java
@@ -0,0 +1,31 @@
+package flab.rocket_market.products.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Categories {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+}
---
+++ src/main/java/flab/rocket_market/products/entity/ProductsEntityListener.java
@@ -0,0 +1,26 @@
+package flab.rocket_market.products.entity;
+
+import flab.rocket_market.products.service.ProductsSearchService;
+import jakarta.persistence.PostPersist;
+import jakarta.persistence.PostRemove;
+import jakarta.persistence.PostUpdate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductsEntityListener {
+
+    @Autowired
+    private ProductsSearchService productsSearchService;
+
+    @PostPersist
+    @PostUpdate
+    public void postPersistOrUpdate(Products products) {
+        productsSearchService.saveProductsToElasticsearch(products);
+    }
+
+    @PostRemove
+    public void postRemove(Products products) {
+        productsSearchService.deleteProductsFromElasticsearch(products.getProductId());
+    }
+}
