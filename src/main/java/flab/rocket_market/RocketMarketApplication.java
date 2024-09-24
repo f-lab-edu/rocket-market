8cc6c1a2a7cd6375bba55983347cce52b080369d
cheoneunjeong <0102skyjung@naver.com>
2024-09-24 22:45:32 +0900

    [Feature] spring retry 의존성 추가

--- src/main/java/flab/rocket_market/RocketMarketApplication.java
+++ src/main/java/flab/rocket_market/RocketMarketApplication.java
@@ -2,7 +2,9 @@ package flab.rocket_market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class RocketMarketApplication {
 
