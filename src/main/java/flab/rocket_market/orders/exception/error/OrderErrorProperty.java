8a47952203384f9c536bb7d3ed338b3944249abf
cheoneunjeong <0102skyjung@naver.com>
2024-09-24 22:37:42 +0900

    [Feature] 결제 정보, 주문 관련 예외 추가 #76

--- src/main/java/flab/rocket_market/orders/exception/error/OrderErrorProperty.java
+++ src/main/java/flab/rocket_market/orders/exception/error/OrderErrorProperty.java
@@ -10,7 +10,9 @@ import org.springframework.http.HttpStatus;
 public enum OrderErrorProperty implements ErrorProperty {
 
     OUT_OF_STOCK_EXCEPTION(HttpStatus.BAD_REQUEST, "재고가 부족합니다."),
-    PAYMENT_PROCESSING_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "결제에 실패하였습니다.");
+    PAYMENT_PROCESSING_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "결제 API 호출에 실패하였습니다."),
+    PAYMENT_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "결제 정보가 없습니다."),
+    ORDER_FAILED_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "주문 데이터 저장에 실패하였습니다.");
 
     private final HttpStatus status;
     private final String message;
